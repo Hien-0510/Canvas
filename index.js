37281980d80b050502672128dc87447b81fe8511
Cupid <hien.lm1452@sinhvien.hoasen.edu.vn>
2023-07-25 22:25:00 +0700

    Canvas Update2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ pen.canvas.style.border = '10px solid black';
 
     function drawSmileFace(dx, dy, size, skin){
         pen.beginPath();
-        pen.arc(dx, dy, size, Math.PI/2, 3 * Math.PI/2);
+        pen.arc(dx, dy, size, 0,  2*Math.PI);
         pen.fillStyle = skin;
         pen.stroke();
         pen.fill();
@@ -22,12 +22,36 @@ pen.canvas.style.border = '10px solid black';
 
     drawSmileFace(W/2, H/2, 200, "red");
 
-    function loop(){
-        const FPS = 60;
-        let timeLoop = 1000/FPS; // Thời gian 1 frame
+    function loop() {
+        const FPS = 120; 
+        let timeLoop = 1000 / FPS; 
         let size = 200;
+        let dy = H / 2;
+        let dx = W / 2;
+        let colors = ["red", "blue", "green", "yellow", "pink", "purple", "orange"];
+        let currentColorIndex = 0;
+        let direction = 1; 
+    
         setInterval(() => {
-            
+            pen.clearRect(0, 0, W, H); 
+            dx += Math.random() * 10 * direction; 
+            dy += Math.random() * 10 * direction; 
+            pen.beginPath();
+            drawSmileFace(dx, dy, size, colors[currentColorIndex]);
+            if (dx >= W || dx <= 0) {
+                direction *= -1;
+                currentColorIndex = (currentColorIndex + 1) % colors.length;
+            }
+    
+            pen.closePath();
         }, timeLoop);
     }
+    
     loop();
+    
+    
+    
+    
+    
+    
+    
